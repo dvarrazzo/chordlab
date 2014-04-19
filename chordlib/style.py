3b234e4c97d6da53d813f148e70498b53b7104e5
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2014-04-19 09:37:27 +0100

    Better errors reporting from bad values in stylesheet

--- chordlib/style.py
+++ chordlib/style.py
@@ -6,8 +6,8 @@ This file is part of chordlab.
 
 import re
 import pkgutil
+import ConfigParser
 from cStringIO import StringIO
-from ConfigParser import ConfigParser
 
 from reportlab.lib import colors
 from reportlab.lib.colors import Color
@@ -16,7 +16,7 @@ from .error import ChordLibError
 
 def get_base_stylesheet():
     """Return as `StyleSheet` populated with a default style."""
-    conf = ConfigParser()
+    conf = ConfigParser.ConfigParser()
     conf.readfp(StringIO(pkgutil.get_data('chordlib', 'data/base.ini')))
     return StyleSheet(conf)
 
@@ -42,44 +42,27 @@ class Style(object):
 
     @property
     def font(self):
-        return self.config.get(self.item, 'font')
+        return self._parse('font')
 
     @property
     def font_size(self):
-        return self.config.getint(self.item, 'font-size')
+        return self._parse_int('font-size')
 
     @property
     def line_height(self):
-        return self.config.getint(self.item, 'line-height')
+        return self._parse_int('line-height')
 
     @property
     def rise(self):
-        return self.config.getint(self.item, 'rise')
+        return self._parse_int('rise')
 
     @property
     def indent(self):
-        return self.config.getint(self.item, 'indent')
+        return self._parse_int('indent')
 
     @property
     def color(self):
-        col = self.config.get(self.item, 'color')
-
-        # color like #FFF
-        m = re.match('#([0-9a-fA-F]{3})', col)
-        if m:
-            return Color(*[int(c, 16) / 15. for c in m.group(1)])
-
-        # color like #FFFFFF
-        m = re.match('#([0-9a-fA-F]{6})', col)
-        if m:
-            return Color(*[int(m.group(1)[c:c+2], 16) / 255.  for c in (0,2,4)])
-
-        # color by name
-        rv = getattr(colors, col, None)
-        if isinstance(rv, Color):
-            return rv
-        else:
-            raise ValueError('bad color: %s' % col)
+        return self._parse_color('color')
 
     LEFT = 'left'
     RIGHT = 'right'
@@ -87,20 +70,69 @@ class Style(object):
 
     @property
     def align(self):
-        align = self.config.get(self.item, 'align').lower()
-        if align == 'left':
-            return self.LEFT
-        elif align == 'right':
-            return self.RIGHT
-        elif align in ('centre', 'center'):
-            return self.CENTER
-        else:
-            raise ValueError('bad align: %s' % align)
+        return self._parse_choices('align',
+            [self.LEFT, self.RIGHT, self.CENTER])
 
     @property
     def scale(self):
-        scale = self.config.get(self.item, 'scale')
-        if '%' in scale:
-            return float(scale.replace('%', '')) * 0.01
+        return self._parse_percent('scale')
+
+
+    def _parse(self, opt):
+        try:
+            return self.config.get(self.item, opt)
+        except ConfigParser.Error, e:
+            raise ChordLibError(str(e))
+
+    def _parse_int(self, opt):
+        val = self._parse(opt)
+        try:
+            return int(val)
+        except ValueError:
+            raise ChordLibError(
+                "bad value for '%s' in section '%s': %s"
+                    % (opt, self.item, val))
+
+    def _parse_color(self, opt):
+        col = self._parse(opt)
+
+        # color like #FFF
+        m = re.match('#([0-9a-fA-F]{3}$)', col)
+        if m:
+            return Color(*[int(c, 16) / 15. for c in m.group(1)])
+
+        # color like #FFFFFF
+        m = re.match('#([0-9a-fA-F]{6})$', col)
+        if m:
+            return Color(
+                *[int(m.group(1)[c:c+2], 16) / 255. for c in (0,2,4)])
+
+        # color by name
+        rv = getattr(colors, col, None)
+        if isinstance(rv, Color):
+            return rv
         else:
-            return float(scale)
+            raise ChordLibError(
+                "bad color in section '%s': %s"
+                    % (self.item, col))
+
+    def _parse_choices(self, opt, choices):
+        val = self._parse(opt)
+        if val.lower() in choices:
+            return val.lower()
+        else:
+            raise ChordLibError(
+                "bad value for '%s' in section %s: %s"
+                    % (opt, self.item, val))
+
+    def _parse_percent(self, opt):
+        val = self._parse(opt)
+        try:
+            if '%' in val:
+                return float(val.replace('%', '')) * 0.01
+            else:
+                return float(val)
+        except ValueError:
+            raise ChordLibError(
+                "bad percent value for '%s' in section '%s': %s"
+                    % (opt, self.item, val))
